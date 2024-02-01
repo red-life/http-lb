703a1c85bbf063605c30a80505f810e45d7a2069
Shayan <r3d.l1fe6@gmail.com>
2024-02-02 00:49:46 +0330

    move rewrite functions from utils to reverse proxy

--- reverseproxy.go
+++ reverseproxy.go
@@ -6,6 +6,27 @@ import (
 	"net/url"
 )
 
+func RewriteHeaders(headers map[string]string) func(*httputil.ProxyRequest) {
+	return func(p *httputil.ProxyRequest) {
+		for key, value := range headers {
+			p.Out.Header.Set(key, value)
+		}
+	}
+}
+
+func RewriteXForwarded(p *httputil.ProxyRequest) {
+	p.Out.Header.Del("X-Forwarded-For")
+	p.Out.Header.Del("X-Forwarded-Host")
+	p.Out.Header.Del("X-Forwarded-Proto")
+	p.SetXForwarded()
+}
+
+func RewriteURL(url *url.URL) func(*httputil.ProxyRequest) {
+	return func(p *httputil.ProxyRequest) {
+		p.SetURL(url)
+	}
+}
+
 func NewReverseProxy(backends []Backend) *CustomReverseProxy {
 	reverseProxy := &CustomReverseProxy{}
 	for _, b := range backends {
--- utils.go
+++ utils.go
@@ -4,8 +4,6 @@ import (
 	"hash/fnv"
 	"net"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 )
 
 func Hash(input string) uint {
@@ -28,25 +26,4 @@ func CreateTransport(options TransportOptions) *http.Transport {
 	}
 	tr.DialContext = dialer.DialContext
 	return tr
-}
-
-func RewriteHeaders(headers map[string]string) func(*httputil.ProxyRequest) {
-	return func(p *httputil.ProxyRequest) {
-		for key, value := range headers {
-			p.Out.Header.Set(key, value)
-		}
-	}
-}
-
-func RewriteXForwarded(p *httputil.ProxyRequest) {
-	p.Out.Header.Del("X-Forwarded-For")
-	p.Out.Header.Del("X-Forwarded-Host")
-	p.Out.Header.Del("X-Forwarded-Proto")
-	p.SetXForwarded()
-}
-
-func RewriteURL(url *url.URL) func(*httputil.ProxyRequest) {
-	return func(p *httputil.ProxyRequest) {
-		p.SetURL(url)
-	}
 }
