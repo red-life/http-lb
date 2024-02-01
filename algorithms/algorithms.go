89c83deb77a8356c40bd28fc4f469b2aa2c490e8
Shayan <r3d.l1fe6@gmail.com>
2024-02-01 19:28:02 +0330

    move LoadBalancingAlgorithm inteface to definitions

--- algorithms/algorithms.go
+++
@@ -1,9 +0,0 @@
-package algorithms
-
-import (
-	"github.com/red-life/http-lb"
-)
-
-type LoadBalancingAlgorithm interface {
-	ChooseBackend(r http_lb.Request) string
-}
--- definitions.go
+++ definitions.go
@@ -1,5 +1,9 @@
 package http_lb
 
+type LoadBalancingAlgorithm interface {
+	ChooseBackend(r Request) string
+}
+
 type Request struct {
 	RemoteIP string
 	URLPath  string
