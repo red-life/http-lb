1fbf8f59ec50788ea418d0039e5264405af10a61
Shayan <r3d.l1fe6@gmail.com>
2024-02-01 20:53:52 +0330

    add frontend

---
+++ frontend.go
@@ -0,0 +1,35 @@
+package http_lb
+
+import "net/http"
+
+func NewFrontend(listenAddr string, tls *TLSOptions) *Frontend {
+	return &Frontend{
+		listenAddr: listenAddr,
+		mux:        http.NewServeMux(),
+		tls:        tls,
+	}
+}
+
+type TLSOptions struct {
+	CertFile string
+	KeyFile  string
+}
+
+type Frontend struct {
+	listenAddr   string
+	mux          *http.ServeMux
+	tls          *TLSOptions
+	reqForwarder RequestForwarder
+}
+
+func (f *Frontend) Handler(rw http.ResponseWriter, r *http.Request) {
+	f.reqForwarder.Forward(rw, r)
+}
+
+func (f *Frontend) Run() error {
+	f.mux.HandleFunc("/", f.Handler)
+	if f.tls != nil {
+		return http.ListenAndServeTLS(f.listenAddr, f.tls.CertFile, f.tls.KeyFile, f.mux)
+	}
+	return http.ListenAndServe(f.listenAddr, f.mux)
+}
