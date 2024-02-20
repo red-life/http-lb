c2060dfc59e47408ea449001c237e2ce20cdee79
Shayan <r3d.l1fe6@gmail.com>
2024-02-20 22:52:00 +0330

    add configuration parser and validator

---
+++ cmd/cli/config.go
@@ -0,0 +1,67 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+	"time"
+)
+
+type Config struct {
+	Algorithm   string      `mapstructure:"algorithm" validate:"oneof=round-robin sticky-round-robin weighted-round-robin url-hash ip-hash random"`
+	LogLevel    string      `mapstructure:"log_level" validate:"oneof=debug info warn error"`
+	Frontend    Frontend    `mapstructure:"frontend" validate:"required"`
+	Backend     []Backend   `mapstructure:"backend" validate:"required"`
+	HealthCheck HealthCheck `mapstructure:"health_check" validate:"required"`
+}
+
+type Frontend struct {
+	Listen string `mapstructure:"listen" validate:"required"`
+	TLS    *TLS   `mapstructure:"tls"`
+}
+
+type TLS struct {
+	CertFile string `mapstructure:"cert" validate:"filepath,required"`
+	KeyFile  string `mapstructure:"key" validate:"filepath,required"`
+}
+
+type Backend struct {
+	Address   string        `mapstructure:"address" validate:"url,required"`
+	Timeout   time.Duration `mapstructure:"timeout" validate:"min=1ms,required"`
+	KeepAlive *KeepAlive    `mapstructure:"keep_alive"`
+}
+
+type KeepAlive struct {
+	MaxIdle     int           `mapstrcutre:"max_idle_connections" validate:"min=1,required"`
+	IdleTimeout time.Duration `mapstrcutre:"idle_connection_timeout" validate:"min=1ms,required"`
+}
+
+type HealthCheck struct {
+	Endpoint           string        `mapstructure:"endpoint" validate:"uri,required"`
+	ExpectedStatusCode int           `mapstructure:"expected_status_code" validate:"min=100,max=599,required"`
+	Interval           time.Duration `mapstructure:"interval" validate:"min=1ms,required"`
+	Timeout            time.Duration `mapstructure:"timeout" validate:"min=1ms,required"`
+}
+
+func ParseAndValidateConfig(configFilePath string) (Config, error) {
+	var config Config
+	validate := validator.New()
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(configFilePath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("read in config")
+		return config, err
+	}
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		fmt.Println("unmarshal")
+		return config, err
+	}
+	err = validate.Struct(config)
+	if err != nil {
+		fmt.Println("validate")
+		return config, err
+	}
+	return config, nil
+}
