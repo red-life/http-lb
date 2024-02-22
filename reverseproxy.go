129d881d7709c97f9b045373dc40bf1327a4933e
Shayan <r3d.l1fe6@gmail.com>
2024-02-22 16:44:48 +0330

    implement reverse proxy factory

--- reverseproxy.go
+++ reverseproxy.go
@@ -1,12 +1,11 @@
 package http_lb
 
 import (
-	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
-var _ ReverseProxy = (*CustomReverseProxy)(nil)
+var _ ReverseProxyFactory = (*RPFactory)(nil)
 
 func RewriteXForwarded(p *httputil.ProxyRequest) {
 	p.Out.Header.Del("X-Forwarded-For")
@@ -19,28 +18,38 @@ func RewriteURL(url *url.URL) func(*httputil.ProxyRequest) {
 	}
 }
 
-func NewReverseProxy(backends []Backend) *CustomReverseProxy {
-	reverseProxy := &CustomReverseProxy{
-		cache: make(map[string]*httputil.ReverseProxy),
-	}
-	for _, b := range backends {
-		parsedURL, _ := url.Parse(b.Addr)
-		rewriteURL := RewriteURL(parsedURL)
-		rp := &httputil.ReverseProxy{}
-		rp.Rewrite = func(request *httputil.ProxyRequest) {
-			RewriteXForwarded(request)
-			rewriteURL(request)
-		}
-		rp.Transport = b.Transport
-		reverseProxy.cache[b.Addr] = rp
+func NewRPFactory(transportFactory TransportFactory, reWriters ...func(request *httputil.ProxyRequest)) *RPFactory {
+	reverseProxy := &RPFactory{
+		cache:            make(map[string]*httputil.ReverseProxy),
+		reWriters:        reWriters,
+		transportFactory: transportFactory,
 	}
 	return reverseProxy
 }
 
-type CustomReverseProxy struct {
-	cache map[string]*httputil.ReverseProxy
+type RPFactory struct {
+	cache            map[string]*httputil.ReverseProxy
+	reWriters        []func(request *httputil.ProxyRequest)
+	transportFactory TransportFactory
 }
 
-func (rp *CustomReverseProxy) ServeHTTP(backendAddr string, rw http.ResponseWriter, r *http.Request) {
-	rp.cache[backendAddr].ServeHTTP(rw, r)
+func (rp *RPFactory) Create(backendAddr string) (*httputil.ReverseProxy, error) {
+	if proxy, ok := rp.cache[backendAddr]; ok {
+		return proxy, nil
+	}
+	proxy := &httputil.ReverseProxy{}
+	if len(rp.reWriters) > 0 {
+		proxy.Rewrite = func(request *httputil.ProxyRequest) {
+			for _, reWriter := range rp.reWriters {
+				reWriter(request)
+			}
+		}
+	}
+	transport, err := rp.transportFactory.Create(backendAddr)
+	if err != nil {
+		return nil, err
+	}
+	proxy.Transport = transport
+	rp.cache[backendAddr] = proxy
+	return proxy, nil
 }
