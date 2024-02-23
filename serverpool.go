cef240bbe27b54dd531c23c7f6dfb6e91e558fe4
Shayan <r3d.l1fe6@gmail.com>
2024-02-23 07:47:49 +0330

    fix: use slices of Server to fix unordered servers

--- serverpool.go
+++ serverpool.go
@@ -8,32 +8,35 @@ import (
 var _ ServerPool = (*ServerPoolImplementation)(nil)
 
 func NewServerPool(servers []string, logger *zap.Logger) *ServerPoolImplementation {
-	s := make(map[string]ServerStatus)
+	serverPool := &ServerPoolImplementation{
+		serverAddrsIndex: make(map[string]int),
+		logger:           logger,
+	}
 	for _, server := range servers {
-		s[server] = Healthy
+		serverPool.RegisterServer(server)
 	}
-	return &ServerPoolImplementation{
-		servers: s,
-		logger:  logger,
-	}
+	return serverPool
 }
 
 type ServerPoolImplementation struct {
-	servers map[string]ServerStatus
-	rwLock  sync.RWMutex
-	logger  *zap.Logger
+	servers          []Server
+	serverAddrsIndex map[string]int
+	rwLock           sync.RWMutex
+	logger           *zap.Logger
 }
 
 func (b *ServerPoolImplementation) SetServerStatus(server string, status ServerStatus) error {
-	if _, ok := b.servers[server]; !ok {
+	if !b.checkExistence(server) {
 		return ErrServerNotExist
 	}
-	b.servers[server] = status
+	b.rwLock.Lock()
+	defer b.rwLock.Unlock()
+	b.servers[b.serverAddrsIndex[server]].Status = status
 	return nil
 }
 
-func (b *ServerPoolImplementation) Servers() []string {
-	return KeysMap(b.servers)
+func (b *ServerPoolImplementation) Servers() []Server {
+	return CopySlice(b.servers)
 }
 
 func (b *ServerPoolImplementation) HealthyServers() []string {
@@ -46,33 +49,49 @@ func (b *ServerPoolImplementation) UnhealthyServers() []string {
 
 func (b *ServerPoolImplementation) getByStatus(status ServerStatus) []string {
 	servers := make([]string, 0)
-	for server, serverStatus := range b.servers {
-		if serverStatus == status {
-			servers = append(servers, server)
+	for _, server := range b.servers {
+		if server.Status == status {
+			servers = append(servers, server.Address)
 		}
 	}
 	return servers
 }
 
 func (b *ServerPoolImplementation) RegisterServer(server string) error {
-	b.rwLock.RLock()
-	if _, ok := b.servers[server]; ok {
+	if b.checkExistence(server) {
 		return ErrServerExists
 	}
-	b.rwLock.RUnlock()
 	b.rwLock.Lock()
 	defer b.rwLock.Unlock()
-	b.servers[server] = Healthy
+	b.servers = append(b.servers, Server{
+		Address: server,
+		Status:  Healthy,
+	})
+	b.serverAddrsIndex[server] = len(b.servers) - 1
 	b.logger.Debug("server registered", zap.String("server", server))
 	return nil
 }
 
 func (b *ServerPoolImplementation) UnregisterServer(server string) error {
-	b.rwLock.Lock()
-	defer b.rwLock.Unlock()
-	if _, ok := b.servers[server]; !ok {
+	if !b.checkExistence(server) {
 		return ErrServerNotExist
 	}
-	delete(b.servers, server)
+	b.rwLock.Lock()
+	defer b.rwLock.Unlock()
+	idx := b.serverAddrsIndex[server]
+	b.servers = append(b.servers[:idx], b.servers[idx+1:]...)
+	delete(b.serverAddrsIndex, server)
+	for s := range b.serverAddrsIndex {
+		if b.serverAddrsIndex[s] > idx {
+			b.serverAddrsIndex[s]--
+		}
+	}
 	return nil
+}
+
+func (b *ServerPoolImplementation) checkExistence(server string) bool {
+	b.rwLock.RLock()
+	defer b.rwLock.RUnlock()
+	_, ok := b.serverAddrsIndex[server]
+	return ok
 }
